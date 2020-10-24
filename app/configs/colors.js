c55e1d2f36a452e0273b258dbc8ec021f8da4317
Kushen De Silva <kushenthimira@gmail.com>
2020-10-25 00:23:58 +0530

    welcomeScreen v1.1

---
+++ app/configs/colors.js
@@ -0,0 +1,6 @@
+export default {
+  patientPrimary: "#3d4db7",
+  doctorPrimary: "#009687",
+  cooperationPrimary: "#eb1460",
+  background: "white",
+};
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,40 +1,59 @@
 import React from "react";
 import { Image, StyleSheet, View, Text } from "react-native";
 
+import colors from "../configs/colors";
+
 function WelcomeScreen(props) {
   return (
     <View style={styles.background}>
       <View style={styles.logoContainer}>
         <Image source={require("../assets/logo.png")} style={styles.logo} />
-        <Text>Ayushaadi Medical App</Text>
+        <Text style={styles.logoText}>Ayushaadi Medical App</Text>
+        <Text>by Software Troopers</Text>
       </View>
-      <View style={styles.patientButton}></View>
-      <View style={styles.doctorButton}></View>
-      <View style={styles.cooperationButton}></View>
+      <View style={styles.patientButton}>
+        <Text style={styles.buttonText}>Patient</Text>
+      </View>
+      <View style={styles.doctorButton}>
+        <Text style={styles.buttonText}>Doctor</Text>
+      </View>
+      <View style={styles.cooperationButton}>
+        <Text style={styles.buttonText}>Cooperation</Text>
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: "white",
+    backgroundColor: colors.background,
     flex: 1,
     justifyContent: "flex-end",
     alignItems: "center",
   },
   patientButton: {
-    width: "100%",
+    width: "90%",
     height: 70,
-    backgroundColor: "#3d4db7",
+    backgroundColor: colors.patientPrimary,
+    borderRadius: 100,
+    margin: 10,
+    elevation: 10,
   },
   doctorButton: {
-    width: "100%",
+    width: "90%",
     height: 70,
-    backgroundColor: "#009687",
+    backgroundColor: colors.doctorPrimary,
+    borderRadius: 100,
+    margin: 10,
+    elevation: 10,
   },
   cooperationButton: {
-    width: "100%",
+    width: "90%",
     height: 70,
-    backgroundColor: "#eb1460",
+    backgroundColor: colors.cooperationPrimary,
+    borderRadius: 100,
+    margin: 10,
+    marginBottom: 20,
+    elevation: 10,
   },
   logo: {
     width: 100,
@@ -42,8 +61,20 @@ const styles = StyleSheet.create({
   },
   logoContainer: {
     position: "absolute",
-    top: 70,
+    top: 80,
     alignItems: "center",
+  },
+  logoText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "red",
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold",
+    alignSelf: "center",
+    marginVertical: 20,
   },
 });
 
