4efa3f612dc2306cfed0ccbc1e10a9166e78625b
K.A.N.Tharushka <tharushka.nt@gmail.com>
2020-11-28 16:49:40 +0530

    navigation v1.1

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
+import AuthNavigator from "./app/navigation/AuthNavigation";
 import DatePicker from "./app/screens/DatePicker";
 import DoctorAccInfo from "./app/screens/DoctorAccInfo";
 import PatientLogin from "./app/screens/PatientLogin";
 import TestExample from "./app/screens/TestExample";
 
 export default function App() {
-  return <TestExample />;
+  return <NavigationContainer>
+    <AuthNavigator/>
+  </NavigationContainer>;
 }
