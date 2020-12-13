d45f0361d50ad214d24e1d34a3bcbf1f12ab739d
Kushen De Silva <kushenthimira@gmail.com>
2020-12-13 15:24:08 +0530

    frontEndCompletion v1.6

--- app/screens/Patient/PatientCategoryDetails.js
+++ app/screens/Patient/PatientCategoryDetails.js
@@ -1,129 +1,143 @@
-import React from 'react';
-import { View,FlatList , Text, StyleSheet,ImageBackground, ScrollView,TouchableOpacity } from 'react-native';
+import React from "react";
+import {
+  View,
+  FlatList,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 
-import colors from '../../configs/colors'
-import AppText from '../../components/AppText'
+import colors from "../../configs/colors";
+import AppText from "../../components/AppText";
 import CardPatient from "../../components/CardPatient";
-import ScreenVarient from '../../components/ScreenVarient';
-import AppSearchBar from '../../components/AppSearchBar';
+import ScreenVarient from "../../components/ScreenVarient";
+import AppSearchBar from "../../components/AppSearchBar";
 
-const cardList =[
-   {
-       id:1,
-       image:require('../../assets/doc.png'),
-       title:'Dr.Anonymous',
-       priceaudio:'1300',
-       pricevideo:'1500',
-   },
-   {
-    id:2,
-    image:require('../../assets/categoryDoc2.png'),
-    title:'Dr.Anonymous',
-    priceaudio:'1300',
-    pricevideo:'1500',
-    },
-    {
-    id:3,
-    image:require('../../assets/categoryDoc3.png'),
-    title:'Dr.Anonymous',
-    priceaudio:'1300',
-    pricevideo:'1500',
-    },
-]
+const cardList = [
+  {
+    id: 1,
+    image: require("../../assets/doc.png"),
+    title: "Dr.Anonymous",
+    priceaudio: "1300",
+    pricevideo: "1500",
+  },
+  {
+    id: 2,
+    image: require("../../assets/categoryDoc2.png"),
+    title: "Dr.Anonymous",
+    priceaudio: "1300",
+    pricevideo: "1500",
+  },
+  {
+    id: 3,
+    image: require("../../assets/categoryDoc3.png"),
+    title: "Dr.Anonymous",
+    priceaudio: "1300",
+    pricevideo: "1500",
+  },
+];
 
-function PatientCategoryDetails({navigation}) {
-    return (
-        <ScreenVarient>
-                <AppSearchBar></AppSearchBar>
-            <ScrollView style={styles.screen}>
-                <View style={styles.containerTop}>
-                    <AppText style={styles.heading}>Psychologist</AppText>
-                    <View style={styles.topDetails}>
-                            <FlatList numColumns={3}
-                            data={cardList}
-                            keyExtractor={listing => listing.id.toString()}
-                            renderItem={({item})=>
-                                <CardPatient
-                                title={item.title}
-                                image={item.image}
-                                priceaudio={"LKR" + item.priceaudio}
-                                pricevideo={"LKR" + item.pricevideo}
-                                />
-                            }
-                            />
-                    </View>
-                            <TouchableOpacity style={styles.viewAll}>
-                                <AppText style={styles.viewText}>View All</AppText>
-                            </TouchableOpacity>
-                </View>
+function PatientCategoryDetails({ navigation }) {
+  return (
+    <ScreenVarient>
+      <AppSearchBar></AppSearchBar>
+      <ScrollView style={styles.screen}>
+        <View style={styles.containerTop}>
+          <AppText style={styles.heading}>Psychologist</AppText>
+          <View style={styles.topDetails}>
+            <FlatList
+              numColumns={3}
+              contentContainerStyle={{ alignItems: "center" }}
+              data={cardList}
+              keyExtractor={(listing) => listing.id.toString()}
+              renderItem={({ item }) => (
+                <CardPatient
+                  title={item.title}
+                  image={item.image}
+                  priceaudio={"LKR" + item.priceaudio}
+                  pricevideo={"LKR" + item.pricevideo}
+                />
+              )}
+            />
+          </View>
+          <TouchableOpacity style={styles.viewAll}>
+            <AppText style={styles.viewText}>View All</AppText>
+          </TouchableOpacity>
+        </View>
 
-                <View style={styles.containerTop}>
-                    <AppText style={styles.heading}>Cardiologist </AppText>
-                    <View style={styles.topDetails}>
-                            <FlatList numColumns={3}
-                            data={cardList}
-                            keyExtractor={listing => listing.id.toString()}
-                            renderItem={({item})=>
-                                <CardPatient
-                                title={item.title}
-                                image={item.image}
-                                priceaudio={"LKR" + item.priceaudio}
-                                pricevideo={"LKR" + item.pricevideo}
-                                />
-                            }
-                            />
-                    </View>
-                            <TouchableOpacity style={styles.viewAll}>
-                                <AppText style={styles.viewText}>View All</AppText>
-                            </TouchableOpacity>
-                </View>
+        <View style={styles.containerTop}>
+          <AppText style={styles.heading}>Cardiologist </AppText>
+          <View style={styles.topDetails}>
+            <FlatList
+              contentContainerStyle={{ alignItems: "center" }}
+              numColumns={3}
+              data={cardList}
+              keyExtractor={(listing) => listing.id.toString()}
+              renderItem={({ item }) => (
+                <CardPatient
+                  title={item.title}
+                  image={item.image}
+                  priceaudio={"LKR" + item.priceaudio}
+                  pricevideo={"LKR" + item.pricevideo}
+                />
+              )}
+            />
+          </View>
+          <TouchableOpacity style={styles.viewAll}>
+            <AppText style={styles.viewText}>View All</AppText>
+          </TouchableOpacity>
+        </View>
 
-                <View style={styles.containerTop}>
-                    <AppText style={styles.heading}>Dental</AppText>
-                    <View style={styles.topDetails}>
-                            <FlatList numColumns={3}
-                            data={cardList}
-                            keyExtractor={listing => listing.id.toString()}
-                            renderItem={({item})=>
-                                <CardPatient
-                                title={item.title}
-                                image={item.image}
-                                priceaudio={"LKR" + item.priceaudio}
-                                pricevideo={"LKR" + item.pricevideo}
-                                />
-                            }
-                            />
-                    </View>
-                            <TouchableOpacity style={styles.viewAll}>
-                                <AppText style={styles.viewText}>View All</AppText>
-                            </TouchableOpacity>
-                </View>
-            </ScrollView>
-        </ScreenVarient>
-    );
+        <View style={styles.containerTop}>
+          <AppText style={styles.heading}>Dental</AppText>
+          <View style={styles.topDetails}>
+            <FlatList
+              contentContainerStyle={{ alignItems: "center" }}
+              numColumns={3}
+              data={cardList}
+              keyExtractor={(listing) => listing.id.toString()}
+              renderItem={({ item }) => (
+                <CardPatient
+                  title={item.title}
+                  image={item.image}
+                  priceaudio={"LKR" + item.priceaudio}
+                  pricevideo={"LKR" + item.pricevideo}
+                />
+              )}
+            />
+          </View>
+          <TouchableOpacity style={styles.viewAll}>
+            <AppText style={styles.viewText}>View All</AppText>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </ScreenVarient>
+  );
 }
 const styles = StyleSheet.create({
-    screen:{
-        flex:1,
-    },
-    heading:{
-        fontWeight:'bold',
-        fontSize:20,
-        marginLeft:"5%",
-        marginBottom:"2%",
-    },
-    topDetails:{
-        backgroundColor:colors.patientPrimary,
-        flexDirection:"row",
-        padding:10,
-    },
-    viewAll:{
-        alignItems:'flex-end'
-    },
-    viewText:{
-        fontWeight:'bold',
-        color:colors.patientPrimary,
-    },
-  });
+  screen: {
+    flex: 1,
+  },
+  heading: {
+    fontWeight: "bold",
+    fontSize: 20,
+    marginLeft: "5%",
+    marginBottom: "2%",
+  },
+  topDetails: {
+    backgroundColor: colors.patientPrimary,
+    flexDirection: "row",
+    padding: 10,
+  },
+  viewAll: {
+    alignItems: "flex-end",
+  },
+  viewText: {
+    fontWeight: "bold",
+    color: colors.patientPrimary,
+  },
+});
 
 export default PatientCategoryDetails;
--- app/screens/Patient/PatientCategoryModal.js
+++ app/screens/Patient/PatientCategoryModal.js
@@ -1,72 +1,69 @@
-import React from 'react';
-import { View ,StyleSheet , FlatList,
-    Modal} from 'react-native';
+import React from "react";
+import { View, StyleSheet, FlatList, Modal } from "react-native";
 
-import AppButton from '../../components/AppButton';
-import ProfileCard from '../../components/ProfileCard';
-import ScreenVarient from '../../components/ScreenVarient';
+import AppButton from "../../components/AppButton";
+import ProfileCard from "../../components/ProfileCard";
+import ScreenVarient from "../../components/ScreenVarient";
 
 const list = [
-    {
-        id:"1",
-        image:require("../../assets/test.jpg"),
-        name:"Dr.Anonymous",
-        profession:"App Development",
-        education:"Computing",
-        university:"Coventry",
-        language:"sinhala / English",
-        Callprice:"LKR 1300 upward",
-        Videoprice:"LKR 2000 upward"
-    }
-]
+  {
+    id: "1",
+    image: require("../../assets/test.jpg"),
+    name: "Dr.Anonymous",
+    profession: "App Development",
+    education: "Computing",
+    university: "Coventry",
+    language: "sinhala / English",
+    Callprice: "LKR 1300 upward",
+    Videoprice: "LKR 2000 upward",
+  },
+];
 function PatientCategoryModal(props) {
-    const [modalVisible, setModalVisible] = useState(false);
-    return (
-       <ScreenVarient>
-           <View style={styles.screen} >
-           <Modal animationType="slide" transparent={true} visible={modalVisible}>
-               <FlatList
-                data={list}
-                keyExtractor={list => list.id.toString()}
-                renderItem={({item}) =>
-               <ProfileCard
-                    image={item.image}
-                    name={item.name}
-                    profession = {item.profession}
-                    education= {item.education}
-                    university={item.university}
-                    language={item.language}
-                    Callprice={item.Callprice}
-                    Videoprice={item.Videoprice}
-               
-                    IconButton={
-                        <View style={styles.buttonContainer}>
-                            <AppButton title="Book Now" color="black" />
-                        </View>
-                    }
-               />
-            }
-               />
-               </Modal>
-           </View>
-       </ScreenVarient>
-    );
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <ScreenVarient>
+      <View style={styles.screen}>
+        <Modal animationType="slide" transparent={true} visible={modalVisible}>
+          <FlatList
+            data={list}
+            keyExtractor={(list) => list.id.toString()}
+            renderItem={({ item }) => (
+              <ProfileCard
+                image={item.image}
+                name={item.name}
+                profession={item.profession}
+                education={item.education}
+                university={item.university}
+                language={item.language}
+                Callprice={item.Callprice}
+                Videoprice={item.Videoprice}
+                IconButton={
+                  <View style={styles.buttonContainer}>
+                    <AppButton title="Book Now" color="black" />
+                  </View>
+                }
+              />
+            )}
+          />
+        </Modal>
+      </View>
+    </ScreenVarient>
+  );
 }
 
 const styles = StyleSheet.create({
-    screen:{
-        flex:1,
-        alignItems:"center",
-        justifyContent:"center",
-        padding:18,
-        alignContent:"center",
-        flexDirection:"row"
-    },
-    buttonContainer:{
-        width: "100%",
-        height: 50,
-        marginTop:-20
-    },
-       
-})
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 18,
+    alignContent: "center",
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    width: "100%",
+    height: 50,
+    marginTop: -20,
+  },
+});
 export default PatientCategoryModal;
