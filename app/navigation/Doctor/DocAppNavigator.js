0eab3fac2de7d450d6470da3d72f751627578098
Kushen De Silva <60555833+kushenthimira@users.noreply.github.com>
2021-01-26 04:00:36 +0530

    doctorNavigation v1.0

--- app/navigation/Doctor/DocAppNavigator.js
+++ app/navigation/Doctor/DocAppNavigator.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 
@@ -13,16 +13,20 @@ import DoctorEditDate from "../../screens/Doctor/DoctorEditDate";
 import EditNavigator from "./EditNavigator";
 import DocAccNavigator from "./DocAccNavigator";
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const DocAppNavigator = () => (
-  <Tab.Navigator>
+  <Tab.Navigator
+    screenOptions={{
+      tabBarColor: "#009687",
+    }}
+  >
     <Tab.Screen
       name="Home"
       component={DocTopNavigator}
       options={{
         tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="home" color={"#009687"} size={size} />
+          <MaterialCommunityIcons name="home" color={"white"} size={24} />
         ),
       }}
     />
@@ -32,21 +36,18 @@ const DocAppNavigator = () => (
       component={DoctorAppointment}
       options={{
         tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons
-            name="calendar"
-            color={"#009687"}
-            size={size}
-          />
+          <MaterialCommunityIcons name="calendar" color={"white"} size={24} />
         ),
       }}
     />
 
     <Tab.Screen
-      name=" "
+      name="Edit"
       component={EditNavigator}
       options={{
+        title: "",
         tabBarIcon: ({ color, size }) => (
-          <AntDesign name="pluscircleo" color={"#009687"} size={size} />
+          <AntDesign name="pluscircleo" color={"white"} size={24} />
         ),
       }}
     />
@@ -58,8 +59,8 @@ const DocAppNavigator = () => (
         tabBarIcon: ({ color, size }) => (
           <MaterialCommunityIcons
             name="view-grid-plus"
-            color={"#009687"}
-            size={size}
+            color={"white"}
+            size={24}
           />
         ),
       }}
@@ -72,8 +73,8 @@ const DocAppNavigator = () => (
         tabBarIcon: ({ color, size }) => (
           <MaterialCommunityIcons
             name="account-box"
-            color={"#009687"}
-            size={size}
+            color={"white"}
+            size={24}
           />
         ),
       }}
